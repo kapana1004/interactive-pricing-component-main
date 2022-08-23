4a1603859b445d533297cf27aec960965c654dfd
Mac <mac@Macs-MacBook-Pro.local>
2022-08-24 03:57:55 +0400

    added function with eventlistener for check input to change the content of monhly and yearly payment

--- slider.js
+++ slider.js
@@ -19,13 +19,13 @@ discount.addEventListener("input", function(){
     //slideDesktop.value.innerHTML==1 არ ბრუნდება სლაიდერი საწყის პოზიციაში
 
 
-  }else if(discount){
+  }else if(!discount.checked){
 
     pageViewInput.innerHTML="10K " + "pageviews".toUpperCase();
     monthPriceMob.innerHTML="$8"
     pagviewDEsktop.innerHTML="10K " + "pageviews".toUpperCase();
     monthPrice.innerHTML="$8"
-    //priceSlider.value.innerHTML==1
+    priceSlider.value.innerHTML==1;
 
   }
 
@@ -35,7 +35,12 @@ discount.addEventListener("input", function(){
 priceSlider.addEventListener("input", function(){
 
 
+if(priceSlider.value==0 &&!discount.checked){
 
+        pageViewInput.innerHTML="0 " + "pageviews".toUpperCase();
+        monthPriceMob.innerHTML="$0.00"
+    
+    }
 if(priceSlider.value==1 &&!discount.checked){
 
     pageViewInput.innerHTML="10K " + "pageviews".toUpperCase();
@@ -170,6 +175,11 @@ slideDesktop.addEventListener("input", function(event){
 
 
 
+//set the div to slider with pos. absolute and give 1px width. 
+//in js adding eventlistener on slider and give him function: 
+//with for loop catch the new div`s width  
+    //let i = newDiv.style.width.length;
+    //for("i=1, i<=100, i++"){newDiv.style.background-color:rgba(164, 243, 235, 1)}
 
 
 
@@ -192,56 +202,3 @@ slideDesktop.addEventListener("input", function(event){
 
 
 
-
-// const range = document.getElementById('range');
-// const pageviewsCount = document.getElementById('pageviews-count');
-// const toggleDiscount = document.getElementById('toggle_discount');
-// const price = document.getElementById('price');
-
-// function handlePricingChanges(value) {
-// 	let pricing = 8;
-
-// 	if ((value > 0) & (value <= 20)) {
-// 		pageviewsCount.textContent = '10K Pageviews';
-// 		pricing = 8;
-// 	} else if (value > 20 && value <= 40) {
-// 		pageviewsCount.textContent = '50K Pageviews';
-// 		pricing = 12;
-// 	} else if (value > 40 && value <= 60) {
-// 		pageviewsCount.textContent = '100K Pageviews';
-// 		pricing = 16;
-// 	} else if (value > 60 && value <= 80) {
-// 		pageviewsCount.textContent = '500K Pageviews';
-// 		pricing = 24;
-// 	} else if (value > 80 && value <= 100) {
-// 		pageviewsCount.textContent = '1M Pageviews';
-// 		pricing = 36;
-// 	}
-
-// 	if (toggleDiscount.checked) {
-// 		const totalValue = pricing - pricing * 0.25;
-// 		price.textContent = `$${totalValue}.00`;
-// 	} else {
-// 		price.textContent = `$${pricing}.00`;
-// 	}
-
-	// range.style.background = `linear-gradient(
-	// 	to right,
-	// 	hsl(174, 77%, 80%) 0%,
-	// 	hsl(174, 77%, 80%) ${value}%,
-	// 	 hsl(224, 65%, 95%) 50%,
-	// 	 hsl(224, 65%, 95%) 100%
-	// )`;
-// }
-
-// range.addEventListener('input', function (event) {
-// 	console.log('range value :>> ', event.target.value);
-// 	const value = range.value;
-// 	handlePricingChanges(value);
-// });
-
-// toggleDiscount.addEventListener('input', function (event) {
-// 	console.log('range value :>> ', event.target.value);
-// 	const value = range.value;
-// 	handlePricingChanges(value);
-// });
