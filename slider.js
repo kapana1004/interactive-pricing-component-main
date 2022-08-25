b7cdce05577486c75b375e576d19e6d449e1fa38
Mac <mac@Macs-MacBook-Pro.local>
2022-08-25 20:11:46 +0400

    added new div for color styling slider

--- slider.js
+++ slider.js
@@ -7,6 +7,8 @@ let pagviewDEsktop = document.getElementById("pageviewDesk");
 let monthPrice = document.querySelector(".dollarspan");
 let monthPriceMob = document.querySelector(".dollarspanmob");
 let discount = document.querySelector(".check");
+let greenLsideMob = document.querySelector(".greenslider");
+let greenSlideDesk = document.querySelector(".greensliderdesk");
 
 
 discount.addEventListener("input", function(){
@@ -18,6 +20,8 @@ discount.addEventListener("input", function(){
     monthPrice.innerHTML="$ " + 8*12*0.75+".00";
     slideDesktop.value =1;
     priceSlider.value = 1;
+    greenLsideMob.style.width= "54px";
+    greenSlideDesk.style.width= "86px";
 
 
   }else if(!discount.checked){
@@ -28,6 +32,8 @@ discount.addEventListener("input", function(){
     monthPrice.innerHTML="$8"
     slideDesktop.value =1;
     priceSlider.value=1;
+    greenLsideMob.style.width= "54px";
+    greenSlideDesk.style.width= "86px";
 
   }
 
@@ -44,31 +50,37 @@ if(priceSlider.value==0 &&!discount.checked){
 
         pageViewInput.innerHTML="0 " + "pageviews".toUpperCase();
         monthPriceMob.innerHTML="$0.00"
+        greenLsideMob.style.width= "1px";
     
     }
    else if(priceSlider.value==1 &&!discount.checked){
 
     pageViewInput.innerHTML="10K " + "pageviews".toUpperCase();
     monthPriceMob.innerHTML="$8.00"
+    greenLsideMob.style.width= "54px";
 
 } else if (priceSlider.value==2 &&!discount.checked){
 
     pageViewInput.innerHTML="50K " + "pageviews".toUpperCase()
     monthPriceMob.innerHTML="$12.00 "
+    greenLsideMob.style.width= "106px";
     
 } else if (priceSlider.value==3 &&!discount.checked){
 
     pageViewInput.innerHTML="100K " + "pageviews".toUpperCase()
     monthPriceMob.innerHTML="$16.00 " 
+    greenLsideMob.style.width= "159px";
 } else if (priceSlider.value==4 &&!discount.checked){
 
     pageViewInput.innerHTML="500K " + "pageviews".toUpperCase()
     monthPriceMob.innerHTML="$24.00 " 
+    greenLsideMob.style.width= "212px";
 
 } else if (priceSlider.value==5 &&!discount.checked){
 
     pageViewInput.innerHTML="1M " + "pageviews".toUpperCase()
     monthPriceMob.innerHTML="$36.00 "
+    greenLsideMob.style.width= "264px";
 
 }
 
@@ -76,30 +88,36 @@ if(priceSlider.value==0 &&!discount.checked){
 
     pageViewInput.innerHTML="0 " + "pageviews".toUpperCase();
     monthPriceMob.innerHTML="0.00"
+    greenLsideMob.style.width= "1px";
 }
    else if (priceSlider.value==1 && discount.checked){
 
     pageViewInput.innerHTML="10K " + "pageviews".toUpperCase();
     monthPriceMob.innerHTML="$ " + 8*12*0.75+".00"
+    greenLsideMob.style.width= "54px";
 
 } else if (priceSlider.value==2 && discount.checked){
 
     pageViewInput.innerHTML="50K " + "pageviews".toUpperCase()
     monthPriceMob.innerHTML="$ " + 12*12*0.75+".00"
+    greenLsideMob.style.width= "106px";
     
 } else if (priceSlider.value==3 && discount.checked){
 
     pageViewInput.innerHTML="100K " + "pageviews".toUpperCase()
-    monthPriceMob.innerHTML="$ " + 16*12*0.75+".00" 
+    monthPriceMob.innerHTML="$ " + 16*12*0.75+".00"
+    greenLsideMob.style.width= "159px"; 
 } else if (priceSlider.value==4 && discount.checked){
 
     pageViewInput.innerHTML="500K " + "pageviews".toUpperCase()
     monthPriceMob.innerHTML="$ " + 24*12*0.75+".00" 
+    greenLsideMob.style.width= "212px";
 
 } else if (priceSlider.value==5 && discount.checked){
 
     pageViewInput.innerHTML="1M " + "pageviews".toUpperCase()
     monthPriceMob.innerHTML="$ " + 36*12*0.75+".00"
+    greenLsideMob.style.width= "264px";
 
 }
 
@@ -116,6 +134,7 @@ slideDesktop.addEventListener("input", function(event){
 
         pagviewDEsktop.innerHTML="0 " + "pageviews".toUpperCase();
         monthPrice.innerHTML="$0.00"
+        greenSlideDesk.style.width= "1px";
     
     }
     
@@ -123,31 +142,37 @@ slideDesktop.addEventListener("input", function(event){
 
         pagviewDEsktop.innerHTML="10K " + "pageviews".toUpperCase();
         monthPrice.innerHTML="$8.00"
+        greenSlideDesk.style.width= "86px";
    
     } else if (slideDesktop.value==2){
 
         pagviewDEsktop.innerHTML="50K " + "pageviews".toUpperCase()
         monthPrice.innerHTML="$12.00 "
+        greenSlideDesk.style.width= "172px";
         
     } else if (slideDesktop.value==3){
 
         pagviewDEsktop.innerHTML="100K " + "pageviews".toUpperCase()
         monthPrice.innerHTML="$16.00 " 
+        greenSlideDesk.style.width= "258px";
     } else if (slideDesktop.value==4){
 
         pagviewDEsktop.innerHTML="500K " + "pageviews".toUpperCase()
-        monthPrice.innerHTML="$24.00 " 
+        monthPrice.innerHTML="$24.00 "
+        greenSlideDesk.style.width= "343px"; 
     
     } else if (slideDesktop.value==5){
 
         pagviewDEsktop.innerHTML="1M " + "pageviews".toUpperCase()
         monthPrice.innerHTML="$36.00 "
+        greenSlideDesk.style.width= "428px";
     
     }
     if(slideDesktop.value==0 &&discount.checked){
 
         pagviewDEsktop.innerHTML="0 " + "pageviews".toUpperCase();
         monthPrice.innerHTML="$0.00"
+        greenSlideDesk.style.width= "1px";
     
     }
 
@@ -155,25 +180,30 @@ slideDesktop.addEventListener("input", function(event){
 
         pagviewDEsktop.innerHTML="10K " + "pageviews".toUpperCase();
         monthPrice.innerHTML="$ " + 8*12*0.75+".00"
+        greenSlideDesk.style.width= "86px";
     
     } else if (slideDesktop.value==2 && discount.checked){
     
         pagviewDEsktop.innerHTML="50K " + "pageviews".toUpperCase()
         monthPrice.innerHTML="$ " + 12*12*0.75+".00"
+        greenSlideDesk.style.width= "172px";
         
     } else if (slideDesktop.value==3 && discount.checked){
     
         pagviewDEsktop.innerHTML="100K " + "pageviews".toUpperCase()
         monthPrice.innerHTML="$ " + 16*12*0.75+".00" 
+        greenSlideDesk.style.width= "258px";
     } else if (slideDesktop.value==4 && discount.checked){
     
         pagviewDEsktop.innerHTML="500K " + "pageviews".toUpperCase()
         monthPrice.innerHTML="$ " + 24*12*0.75+".00" 
+        greenSlideDesk.style.width= "343px";
     
     } else if (slideDesktop.value==5 && discount.checked){
     
         pagviewDEsktop.innerHTML="1M " + "pageviews".toUpperCase()
         monthPrice.innerHTML="$ " + 36*12*0.75+".00"
+        greenSlideDesk.style.width= "428px";
     
     }
 
@@ -188,7 +218,8 @@ slideDesktop.addEventListener("input", function(event){
 
 
 
-
+//slider - change default value
+// spot insert image
 
 
 
